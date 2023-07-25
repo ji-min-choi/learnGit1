c4ce6859c9196199373aa6e6de890cd5549ab843
ji-min-choi <4metree@naver.com>
2023-07-25 14:05:01 +0900

    add func print_git

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
+import sys
+
 def pring_message():
 	message = "world"
 	print("hello {}").format(message))
 
+def print_git():
+	message = "from the hell"
+	sys.print("git {}".format(message))
+
+
 if __name__ == "__main__":
 	print_message()
+	print_git()
